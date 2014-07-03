e534ae2722d7eb9f741ed5649c41970f1b12d4df
M. C. McGrath <shidash@shidash.com>
2014-07-03 07:27:17 -0700

    First commit

---
+++ countryconvert.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'countryconvert'
+  s.version     = '0.0.1'
+  s.date        = '2014-07-03'
+  s.summary     = "Converts country codes"
+  s.description = "Converts 2-char ISO country codes to 3-char."
+  s.authors     = ["M. C. McGrath"]
+  s.email       = 'shidash@shidash.com'
+  s.files       = Dir["lib/*"]
+  s.homepage    =
+    'https://github.com/TransparencyToolkit/CountryConvert'
+  s.license       = 'GPL'
+end
---
+++ lib/countryconvert.rb
@@ -0,0 +1,24 @@
+require 'json'
+
+class CountryConvert
+  def initialize(input, countryfield)
+    @input = JSON.parse(input)
+    @countryfield = countryfield
+    @isocodes = JSON.parse(File.read("isocodes.json"))
+  end 
+
+  def convert
+    finaldata = Array.new
+    @input.each do |f|
+      if @isocodes[f[@countryfield].upcase]
+        threeletter = @isocodes[f[@countryfield].upcase]
+        f[@countryfield] = threeletter
+        finaldata.push(f)
+      end
+    end
+    return JSON.pretty_generate(finaldata)
+  end
+end
+
+c = CountryConvert.new(File.read("filtered.json"), "country")
+puts c.convert
